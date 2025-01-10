e7a034cb4f544505f92626f4e08e5a8e31151565
VaVaT1m <v.t.v.04@mail.ru>
2025-01-10 07:49:38 +0300

    Сетка грядок

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,14 +1,60 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    public GameObject spawn;
-    public GameObject obj;
+    /*------------------------------------------
+    //Ётот скрипт будет дл€ создани€ сетки гр€док 
+    --------------------------------------------*/
 
-    private void FixedUpdate()
+    public GameObject obj;
+    public int mode = 0; //»ндекс прокачки, (чек инспектор FieldGrid)
+    int width, height;
+    public float area;// отдалЄнность €чеек сетки друг от друга
+
+    GameObject[] cells;
+
+    void Awake()
     {
-        //Instantiate(obj, spawn.transform.position, Quaternion.identity);
+        switch(mode)
+        {
+            case 0:
+                width = 3;
+                height = 3;
+                break;
+
+            case 1:
+                width = 4;
+                height = 4;
+                break;
+
+            case 2:
+                width = 5;
+                height = 5;
+                break;
+        }
+
+        cells = new GameObject[height * width];
+
+        for (int z = 0, i = 0; z < height; z++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                CreateCell(x, z, i++);
+            }
+        }
+    }
+
+    void CreateCell(int x, int z, int i)
+    {
+        Vector3 position;
+        position.x = x * area;
+        position.y = transform.position.y;
+        position.z = z * area;
+
+        GameObject cell = cells[i] = Instantiate(obj,position, Quaternion.identity);
+        cell.transform.SetParent(transform, false);
     }
 }
