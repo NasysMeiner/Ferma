682115faad06a4eb06f1a272f1e8768c55f11bb6
VaVaT1m <v.t.v.04@mail.ru>
2025-01-11 11:11:35 +0300

    Добавил переключение инструментов

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,4 +1,7 @@
+using System;
 using UnityEngine;
+using UnityEngine.UIElements;
+using UnityEngine.XR;
 using Zenject;
 
 public class Player : MonoBehaviour
@@ -13,6 +16,11 @@ public class Player : MonoBehaviour
 
     private PlayerMove _playerMove;
     private PlayerInput _playerInput;
+
+    GameObject currentItem;
+    int currentItemNum;
+    public GameObject itemSpawner; //Потом поменяй ГО на трансформ
+    public GameObject[] items;
 
     [Inject]
     public void InitPlayer(PlayerMove playerMove, PlayerInput playerInput)
@@ -55,5 +63,45 @@ public class Player : MonoBehaviour
     {
         _playerMove.Rotation();
         _playerInput.ProcessInput();
+
+        ItemsLogic();
+    }
+
+    private void ItemsLogic()
+    {
+        try
+        {
+            if (Input.GetKey("1") || Input.GetKey("2") || Input.GetKey("3") || Input.GetKey("4") || Input.GetKey("5") || Input.GetKey("6"))
+                Items(Convert.ToInt32(Input.inputString));
+        }
+        catch (Exception e)
+        {
+            Debug.Log(Input.inputString);
+        }
+        if(Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+            if(Input.GetAxis("Mouse ScrollWheel") > 0)
+                Items(currentItemNum + 1);
+            else 
+                Items(currentItemNum - 1);
+        }
+    }
+    void Items(int num)//Мешок семян, Бита, Культиватор, Ножницы, Серп, Лейка 
+    {
+        if (num > 6)
+            num = 1;
+        else if (num < 1)
+            num = 6;
+        currentItemNum = num;
+
+        if (currentItem != null)
+            Destroy(currentItem);
+
+        //currentItem = Instantiate(items[num-1], itemSpawner); потом будет это, пока просто буду менять цвет вепона (не забудь поменять ГО на трансформ)
+
+        if(itemSpawner.GetComponent<Renderer>().material.color == Color.green)
+            itemSpawner.GetComponent<Renderer>().material.color = Color.red;
+        else
+            itemSpawner.GetComponent<Renderer>().material.color = Color.green;
     }
 }
